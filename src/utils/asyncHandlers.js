821dfc772bce32eecdad3bfc28fb592efe190c68
Abdullah-Junayed-290 <abdullahjunayed771@gmail.com>
2025-02-22 20:56:45 +0600

    utils setup

--- src/utils/asyncHandlers.js
+++ src/utils/asyncHandlers.js
@@ -1,3 +1,37 @@
-const asyncHandlers = () => {}
 
-export {asyncHandlers};
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve((requestHandler(req, res, next))).catch((err) => next(err))
+  }
+}
+
+export {asyncHandler};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => (req, res, next) => {
+//   try {
+//     await fn(req, res,.next)
+//   } catch (error) {
+//     res.status(err.code || 5000).json({
+//       success: false,
+//       message: err.message
+//     })
+//   }
+// }
---
+++ src/utils/asyncHandlers2.js
@@ -0,0 +1,12 @@
+const asyncHandler = (fn) => (req, res, next) => {
+  try {
+    await fn(req, res,.next)
+  } catch (error) {
+    res.status(err.code || 5000).json({
+      success: false,
+      message: err.message
+    })
+  }
+}
+
+export {asyncHandler};
